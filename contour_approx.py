2c28a6a145df09c2bbf7229d8e9ad5a953012e28
Tyler Holstein <Tylerhols@yahoo.com>
2022-04-08 23:24:29 -0500

    Cropping works with every image condition

--- contour_approx.py
+++ contour_approx.py
@@ -3,19 +3,25 @@ import imutils
 import cv2
 import numpy as np
 
-image = cv2.imread("C:\\Users\\DeathRay3000\\Documents\\Fun Code\\CookieCutter\\guts_01.png")
+image = cv2.imread("/Users/tylerholstein/Documents/Fun Code/CookieCutter/guts_02.jpeg")
+
+cv2.imshow("ORIGINAL PIC", image)
+cv2.waitKey(0)
+
 print("Height:", image.shape[0], "Width:", image.shape[1])
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# canny = cv2.Canny(gray, ) ## try this instead of thresh
 thresh = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY)[1]
 
 cv2.imshow("THRESH", thresh)
 cv2.waitKey(0)
-cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-cnts = imutils.grab_contours(cnts)
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)  # changing the panels has to come from this variable cnts
 c = max(cnts, key=cv2.contourArea)
 output = image.copy()
 
-cv2.drawContours(image, [c], -1, (0, 255, 0), 10)
+cv2.drawContours(output, [c], -1, (0, 255, 0), 10)
 (x, yCoord, w, h) = cv2.boundingRect(c)
 text = "original, num_puts={}".format(len(c))
 cv2.putText(output, text, (x, yCoord - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 0), 2)
@@ -39,29 +45,37 @@ for eps in np.linspace(0.001, 0.05, 10):
     cv2.waitKey(0)
 
     if eps == .05:  # could look at changing this to check for the number of points
-        square = approx
         # print(type(approx))
         currMax, currMin = 0, image.shape[1]
-        data = []
-        arr = np.array(data)
-        # print(data)
-        print(square)
-        for i in square:
+        dataY = []
+        np.array(dataY)
+        print(approx)
+
+        for i in approx:
             for x, y in i:
-                data = np.append(data, y)
-        print(data)
-        newMax = np.max(data)  # these are the lowest and highest points of the image
-        newMin = np.min(data)
-        # print(newMax, newMin)
-        if image.shape[1] - newMax >= 0:  # this won't allow the topHalf value to be a negative value
-            topHalf = image.shape[1] - newMax
+                dataY = np.append(dataY, y)
+        print("data numpy array with", dataY)
+
+        maxY = np.max(dataY)  # these are the lowest and highest points of the image
+        minY = np.min(dataY)
+
+        print("MaxY and MinY", maxY, minY)
+        if image.shape[1] - maxY >= 0:  # this won't allow the topHalf value to be a negative value
+            topHalf = image.shape[1] - maxY
         else:
             topHalf = 0
-        bottomHalf = image.shape[1] - newMin
+        bottomHalf = image.shape[1] - minY
+
         # print(type(topHalf), type(bottomHalf), type(image.shape[1]))
         print(int(topHalf), int(bottomHalf))
-        cropImage = image[int(topHalf): int(bottomHalf), 0: int(image.shape[1])]
-        # cropImage = output[int(newMin) - 40: int(newMax), 0: int(image.shape[1])]
+        if maxY == 0 or maxY < minY:
+            cropImage = image[0: int(image.shape[0]), 0: int(image.shape[1])]  # could have this redo thresh so it
+            # tries again with a different mask
+        else:
+            cropImage = image[int(minY): int(maxY), 0: int(image.shape[1])]
+        print(cropImage)
+
+        # cropImage = output[int(newMin) - 40: int(newMax), : int(image.shape[1])]
         cv2.imshow("Cropped image", cropImage)
         cv2.waitKey(0)
 
