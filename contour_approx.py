076aa85b286346d18a120aeb666f7f9973ff5586
Tyler Holstein <Tylerhols@yahoo.com>
2022-04-12 13:54:56 -0500

    Added proper documentation and comments on functions

--- contour_approx.py
+++ contour_approx.py
@@ -13,6 +13,12 @@ new_mask = 0
 
 
 def create_collage():
+    """
+
+    Returns
+    -------
+    NONE
+    """
     img1 = Image.open(r"images/guts_02.jpeg").convert('RGB')
     img2 = Image.open(r"images/berserkPanel.jpeg")
     x, y = img1.size
@@ -21,58 +27,106 @@ def create_collage():
     img1.show()
 
 
-# TODO
-def image_save(crop_image):  # later turn this into a function that saves the image
-    if os.path.isdir(CROPPED_DIR):
-        print("This exists")
-    else:
+def image_save(crop_image):
+    """
+    Writes cropped image to the cropped directory.
+
+    Creates the cropped directory if it does not exist
+    and takes the name of the current file and adds the string "cropped_"
+    in to the current file name to indicate it is a new image.
+    It then takes the new name and new image and writes it to the cropped directory.
+
+    Parameters
+    ----------
+    crop_image
+
+    Returns
+    -------
+    NONE
+    """
+    if not os.path.isdir(CROPPED_DIR):
         os.mkdir(CROPPED_DIR)
+
     file_name = os.path.basename(IMAGE_NAME)
     new_name = os.path.join(CROPPED_DIR, "cropped_" + file_name)
     cv2.imwrite(new_name, crop_image)
+    # TODO
+    'figure out how to log information of whether it saved or not'
 
 
 def initialize_image():
+    """
+    Grabs files in the images directory and processes them.
+
+    Takes each file, grabs the name of the image, and then reads
+    and sends the image to the function 'thresh_image' for processing.
+
+    Returns
+    -------
+    NONE
+    """
     global IMAGE_NAME
     for file in IMAGE_LIST:
         IMAGE_NAME = os.path.join(IMAGE_DIR, file)
         image = cv2.imread(IMAGE_NAME)
-        # image_show(image, "Original Pic")
         thresh_image(image, 0)
-    # image = cv2.imread(r"images/guts_01.png")
-    # image_show(image, "Original Pic")
-
-    # print("Height:", image.shape[0], "Width:", image.shape[1])
-    # could try cropping the photo so it has to pick a different panel
-    # canny = cv2.Canny(image, 149, 150)  ## try this instead of thresh
 
 
 def thresh_image(image, i):
+    """
+    Takes an image and places image through threshold process
+
+    The image parameter is converted to grayscale
+    and then converted into a new image with threshold.
+    With threshold, we assign pixel values with the given
+    threshold values and then send that new threshold image
+    to the function 'find_contour'
+
+    Parameters
+    ----------
+    image
+    i
+
+    Returns
+    -------
+    NONE
+    """
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # image_show(gray, "GRAY")
     ret, thresh = cv2.threshold(gray, 155, 255, i)
-    # image_show(thresh, "Thresh")
     find_contour(image, thresh)
 
 
 def find_contour(image, thresh):
+    """
+    Finds the contours of the image.
+
+    Determines whether the contour's highest and lowest coordinate
+    are within a difference of 5 points. If they are within the
+    difference of 5 points, it resends the image and applies a
+    new mask through the function 'thresh_image' to try
+    again and crop to a smaller image.
+
+    This allows for the program to try and create a more accurate
+    and smaller cropped image. If the image doesn't produce smaller
+    coordinates, it then saves the image's original state.
+
+    Parameters
+    ----------
+    image
+    thresh
+
+    Returns
+    -------
+    NONE
+    """
     global new_mask
     cnts = cv2.findContours(thresh.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)  # changing the panels has to come from this variable cnts
-
-    print(cv2.contourArea)
     c = max(cnts, key=cv2.contourArea)
-
     output = image.copy()
 
     cv2.drawContours(output, [c], -1, (0, 255, 0), 10)
     (x, yCoord, w, h) = cv2.boundingRect(c)
-    text = "original, num_puts={}".format(len(c))
-    cv2.putText(output, text, (x, yCoord - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 0), 2)
-
-    print("[INFO] {}".format(text))
-    cv2.imshow("Original Contour", output)
-    cv2.waitKey(0)
 
     for eps in np.linspace(0.001, 0.05, 10):
         perimeter = cv2.arcLength(c, True)
@@ -80,45 +134,34 @@ def find_contour(image, thresh):
         output = image.copy()
 
         cv2.drawContours(output, [approx], -1, (0, 0, 0), 3)
-        text = "eps={:4f}, num_pts={}".format(eps, len(approx))
-        cv2.putText(output, text, (x, yCoord - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 0), 2)
-
-        print("[INFO] {}".format(text))
-        # j print(approx)
-        # image_show(output, "Approximated contour")
 
         if eps == .05:  # could look at changing this to check for the number of points
             dataY = []
             np.array(dataY)
-            print(approx)
 
             for i in approx:
                 for x, y in i:
                     dataY = np.append(dataY, y)
-            print("data numpy array with", dataY)
 
             maxY = np.max(dataY)  # these are the lowest and highest points of the image
             minY = np.min(dataY)
 
-            print("MaxY and MinY", maxY, minY)
-            if maxY == 0 or maxY < minY and new_mask < 4:
-                cv2.destroyAllWindows()
-                new_mask = new_mask + 1
-                thresh_image(image, new_mask)
-
-            elif int(image.shape[0]) - 5 <= maxY <= int(image.shape[0]) and new_mask < 4:
-                cv2.destroyAllWindows()
+            # To safeguard against images not being approximated with smaller coordinates
+            # than the overall image. It has it resend the image, so it is converted with
+            # a new threshold type
+            if int(image.shape[0]) - 5 <= maxY <= int(image.shape[0]) and new_mask < 4:
                 new_mask = new_mask + 1
                 thresh_image(image, new_mask)
 
             elif int(image.shape[1]) - 5 <= maxY <= int(image.shape[1]) and new_mask < 4:
-                cv2.destroyAllWindows()
                 new_mask = new_mask + 1
                 thresh_image(image, new_mask)
 
+            # Otherwise, crop the image with the highest and lowest coordinates the image provides
+            # whether the threshold type counter (new_mask) runs out, or if a valid crop image is found
             else:
                 crop_image = image[int(minY): int(maxY), 0: int(image.shape[1])]
-                print(crop_image)
+                # print(crop_image)
                 image_save(crop_image)
 
 
