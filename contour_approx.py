6fd5ab4861cb597713e6984b33cda6129aff9172
tdizzlefizzle1 <Tylerhols@yahoo.com>
2022-04-11 23:40:49 -0500

    Properly create and save cropped images to separate directory

--- contour_approx.py
+++ contour_approx.py
@@ -1,40 +1,57 @@
 from PIL import Image
 import imutils
+import os
 import cv2
 import numpy as np
 
+CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
+IMAGE_DIR = os.path.join(CURRENT_DIR, "images")
+IMAGE_LIST = os.listdir(IMAGE_DIR)
+CROPPED_DIR = os.path.join(CURRENT_DIR, "cropped")
+IMAGE_NAME = ""
 new_mask = 0
 
 
 def create_collage():
-    img1 = Image.open(r"guts_02.jpeg").convert('RGB')
-    img2 = Image.open(r"berserkPanel.jpeg")
+    img1 = Image.open(r"images/guts_02.jpeg").convert('RGB')
+    img2 = Image.open(r"images/berserkPanel.jpeg")
     x, y = img1.size
     print(img1.mode, img2.mode)
     img1.paste(img2, (0, 0))
     img1.show()
 
 
-def image_show(image, window_text):
-    cv2.imshow("{}".format(window_text), image)
-    cv2.waitKey(0)
+# TODO
+def image_save(crop_image):  # later turn this into a function that saves the image
+    if os.path.isdir(CROPPED_DIR):
+        print("This exists")
+    else:
+        os.mkdir(CROPPED_DIR)
+    file_name = os.path.basename(IMAGE_NAME)
+    new_name = os.path.join(CROPPED_DIR, "cropped_" + file_name)
+    cv2.imwrite(new_name, crop_image)
 
 
 def initialize_image():
-    image = cv2.imread(r"guts_01.png")
-    image_show(image, "Original Pic")
+    global IMAGE_NAME
+    for file in IMAGE_LIST:
+        IMAGE_NAME = os.path.join(IMAGE_DIR, file)
+        image = cv2.imread(IMAGE_NAME)
+        # image_show(image, "Original Pic")
+        thresh_image(image, 0)
+    # image = cv2.imread(r"images/guts_01.png")
+    # image_show(image, "Original Pic")
 
-    print("Height:", image.shape[0], "Width:", image.shape[1])
-    thresh_image(image, 0)
+    # print("Height:", image.shape[0], "Width:", image.shape[1])
     # could try cropping the photo so it has to pick a different panel
     # canny = cv2.Canny(image, 149, 150)  ## try this instead of thresh
 
 
 def thresh_image(image, i):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    image_show(gray, "GRAY")
+    # image_show(gray, "GRAY")
     ret, thresh = cv2.threshold(gray, 155, 255, i)
-    image_show(thresh, "Thresh")
+    # image_show(thresh, "Thresh")
     find_contour(image, thresh)
 
 
@@ -68,7 +85,7 @@ def find_contour(image, thresh):
 
         print("[INFO] {}".format(text))
         # j print(approx)
-        image_show(output, "Approximated contour")
+        # image_show(output, "Approximated contour")
 
         if eps == .05:  # could look at changing this to check for the number of points
             dataY = []
@@ -84,28 +101,25 @@ def find_contour(image, thresh):
             minY = np.min(dataY)
 
             print("MaxY and MinY", maxY, minY)
-            if maxY == 0 or maxY < minY:
+            if maxY == 0 or maxY < minY and new_mask < 4:
                 cv2.destroyAllWindows()
                 new_mask = new_mask + 1
                 thresh_image(image, new_mask)
 
-            elif int(image.shape[0]) - 10 <= maxY <= int(image.shape[0]):
+            elif int(image.shape[0]) - 5 <= maxY <= int(image.shape[0]) and new_mask < 4:
                 cv2.destroyAllWindows()
                 new_mask = new_mask + 1
                 thresh_image(image, new_mask)
 
-            elif int(image.shape[1]) - 10 <= maxY <= int(image.shape[1]):
+            elif int(image.shape[1]) - 5 <= maxY <= int(image.shape[1]) and new_mask < 4:
                 cv2.destroyAllWindows()
                 new_mask = new_mask + 1
                 thresh_image(image, new_mask)
-                
+
             else:
-                # cv2.destroyAllWindows()
-                # new_mask = new_mask + 1
-                # thresh_image(image, new_mask)
                 crop_image = image[int(minY): int(maxY), 0: int(image.shape[1])]
                 print(crop_image)
-                image_show(crop_image, "Cropped image")
+                image_save(crop_image)
 
 
 def main():
